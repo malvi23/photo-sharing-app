798ed98f7162ae7d12f19ca7c0d4596d0b2d756f
malvi23 <malvipanchal@gmail.com>
2023-06-01 18:51:18 -0400

    Updated client app structure

--- photo-sharing-app-client/src/app/app-routing.module.ts
+++ photo-sharing-app-client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: 'photos',
+    loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
